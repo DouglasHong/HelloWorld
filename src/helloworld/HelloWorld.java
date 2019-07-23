e8f6ec18718b3f14155fdbecaf9b270d36614565
dougl <dougl@DESKTOP-GETTEAK>
2019-07-22 23:31:47 -0700

    Initial Commit

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author Douglas
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World! "
+                + " another line \n"
+                + " more lines \n"
+                + " awesome \n"
+                + "hello again! \n"
+        );
+
+        System.out.println("Hello\nWorld");
+        System.out.println("xiao"
+                + "\nHui"
+        );
+
+    }
+
+}
